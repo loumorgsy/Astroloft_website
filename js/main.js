354434e4986c58ae77edc240d0a4983bd64365b1
Louis Morgan <louismorgandesign@gmail.com>
2016-12-03 22:03:49 +0000

    responsive menu added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-$(document).ready(function(){
+$( document ).ready(function() {
+  $(".navbar-toggle").on("click", function () {
+    $(".menu-toggle").toggleClass("open");
+  })
 
-	$('.mobile-icon').click( function() {
-		$('nav ul').toggleClass("showing");
 
-	});
+		$(".menu-toggle li a").on('click', function(){
+			$(".menu-toggle").removeClass("open");
 
+
+	})
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
