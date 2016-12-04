909e0fbf354c3dc806336ffbe276a16550c214b2
Paradigm <rob@nulldev.uk>
2016-12-04 19:35:26 +0000

    Fixed work items

--- js/worksliders.js
+++ js/worksliders.js
@@ -1,27 +1,30 @@
 $(".header-branding").click(function(){
     $(".work-header").removeClass("selected");
     $(this).addClass('selected');
-    $(".work-images").addClass('hide').fadeOut();
-    $(".branding").removeClass('hide').fadeIn();
+    $('.work-images').hide();
+    $('.branding').fadeIn('slow');
 });
 
 $(".header-webdesign").click(function(){
     $(".work-header").removeClass("selected");
     $(this).addClass('selected');
-    $(".work-images").addClass('hide');
-    $(".web-design").removeClass('hide');
+    $('.work-images').hide();
+    $('.web-design').removeClass('hide');
+    $('.web-design').fadeIn('slow');
 });
 
 $(".header-appdesign").click(function(){
     $(".work-header").removeClass("selected");
     $(this).addClass('selected');
-    $(".work-images").addClass('hide');
-    $(".app-design").removeClass('hide');
+    $('.work-images').hide();
+    $('.app-design').removeClass('hide');
+    $('.app-design').fadeIn('slow');
 });
 
 $(".header-casestudy").click(function(){
     $(".work-header").removeClass("selected");
     $(this).addClass('selected');
-    $(".work-images").addClass('hide');
-    $(".case-studies").removeClass('hide');
+    $('.work-images').hide();
+    $('.case-studies').removeClass('hide');
+    $('.case-studies').fadeIn('slow');
 });
