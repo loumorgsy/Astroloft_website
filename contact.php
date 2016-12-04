74994f6aa2e49d980f5dfc2347ceb284f7a4f15d
Paradigm <rob@nulldev.uk>
2016-12-04 14:18:29 +0000

    Initial contact PHP

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+
+//Constant value for email
+define("CONTACT_EMAIL", 'info@astroloft.com');
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET')
+{
+  header('Location: http://astroloft.com');
+}
+
+if (isset($_POST['contact-submit']))
+{
+  if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['message']))
+  {
+    die(json_encode(array('status' => 0, 'message' => 'Not all fields have been completed')));
+  }
+  if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+  {
+    die(json_encode(array('status' => 0, 'message' => 'You have not entered a valid email address')));
+  }
+  $headers = "From: webmaster@example.com";
+  $subject = 'Contact Request';
+  mail(CONTACT_EMAIL, $subject , $_POST['message'],$headers);
+  die(json_encode(array('status' => 0, 'message' => 'Thanks for contacting Astroloft')));
+}
