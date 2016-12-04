e91456bb1d92ad1fce35a68c36be1dacf45a1799
Paradigm <rob@nulldev.uk>
2016-12-04 14:26:30 +0000

    Ajax request js

---
+++ js/contact.js
@@ -0,0 +1,33 @@
+  $(document).ready(function(){
+    $('.contact-btn').click(function(e) {
+      $.ajax({
+        url: "contact.php",
+        data:
+        {
+         name: $('input[name="contactname"]').val(),
+         email: $('input[name="email"]').val(),
+         message: $('.message').val(),
+        },
+        type: "POST",
+        dataType: "json",
+        success: function (data)
+        {
+          if (data.success == 1)
+          {
+            alert(data.message);
+          }
+          else if(data.success == 0)
+          {
+            alert(data.message)
+          }
+        },
+
+        error: function (xhr, status)
+        {
+          alert('There was an error');
+        }
+
+      });
+      e.preventDefault();
+    });
+  });
